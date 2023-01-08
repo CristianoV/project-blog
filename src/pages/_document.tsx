1014c6b513f10d3c1e00d711386bcbd1e929b845
CristianoV <cristianoviieira@gmail.com>
2023-01-08 16:56:56 -0300

    feat: icone do site modificado

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,7 @@ export default function Document() {
           href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700;900&display=swap'
           rel='stylesheet'
         />
+                  <link rel="shortcut icon" href="/favicon.png" type="image/png" />
       </Head>
       <body>
         <Main />
