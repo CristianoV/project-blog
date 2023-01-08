62a5c426546759893f641c375e826a56669b2e64
CristianoV <cristianoviieira@gmail.com>
2023-01-08 16:56:19 -0300

    feat:  componente header criado

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,31 @@
+import styles from './styles.module.scss';
+import Image from 'next/image';
+import logo from '../../../public/images/logo.svg';
+
+import Link from 'next/link';
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <a>
+          <Image src={logo} alt='Sujeito Programador Logo' />
+        </a>
+
+        <nav>
+          <Link href='/'>Home</Link>
+          <Link href='/posts'>Conteúdos</Link>
+          <Link href='/sobre'>Quem somos?</Link>
+        </nav>
+
+        <a
+          className={styles.readyButton}
+          type='button'
+          href='https://sujeitoprogramador.com'
+        >
+          COMEÇAR
+        </a>
+      </div>
+    </header>
+  );
+}
