318233c792c344547f16d1983b8f175840639ac2
CristianoV <cristianoviieira@gmail.com>
2023-01-09 20:26:33 -0300

    feat: adiciona um footer para a pagina inicial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
+/* eslint-disable @next/next/no-img-element */
 import Head from 'next/head';
 import styles from '../styles/home.module.scss';
+
+import Image from 'next/image';
+
+import techsImage from '../../public/images/techs.svg';
 
 export default function Home() {
   return (
@@ -47,7 +52,14 @@ export default function Home() {
         </section>
       </div>
 
-
+      <div className={styles.nextLevelContent}>
+        <Image quality={100} src={techsImage} alt="Tecnologias" />
+        <h2>Mais de <span className={styles.alunos}>15 mil</span> já levaram sua carreira ao próximo nivel.</h2>
+        <span>E você vai perder a chance de evoluir de uma vez por todas?</span>
+        <a>
+          <button>ACESSAR TURMA!</button>
+        </a>
+      </div>
     </main>
    </>
   )
